b56bced7e78940a63cd0fe2bf3e7cbaae0ede8f7
marwanLotfy <marwan.lotfy@fixawy.net>
2020-06-28 06:01:49 +0200

    handling SRP in BooksServices and seperate the conceirns of creating and validating

--- domain/services/BookServices.js
+++ domain/services/BookServices.js
@@ -2,19 +2,20 @@ import { BookValidator } from "../../infrastracture";
 import BookRepository from "../../infrastracture/repositories/BookRepository";
 
 export default class BookServices{
+
     constructor(){
         this.bookRepository = new BookRepository;
-        this.bookValidator = new BookValidator;
+        this.validator = new BookValidator;
     }
+
     getBooks(callBack,bookFilter = null){
         this.bookRepository.getBooks(callBack,bookFilter);
     }
-    addBookToStore(book,onSucces,onFail){
-        validator = this.bookValidator.validate(book);
-        if(validator.isValid){
-            onSucces()
-        }else{
-            onFail(validator.errors)
+
+    addBookToStore(book){
+        if(!this.validator.hasErrors()){
+            console.log(book)
         }
     }
+
 }
